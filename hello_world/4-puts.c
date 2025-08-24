91463fa28b9672f225c55a535f17ee1a05689ef8
root <root@ip-10-42-182-248.ec2.internal>
2025-08-24 12:09:10 +0000

    updates

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * return 0
  */
-int main(void) /*this is main function*/
+int main(void) /**this is main function*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); /* this function will return 0 value*/
+	return (0); /** this function will return 0 value*/
 }
