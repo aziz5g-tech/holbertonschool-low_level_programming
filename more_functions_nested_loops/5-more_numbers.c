811d16adbf4ed82d459cdc2d0eec8d30ccdf249b
root <root@ip-10-42-154-163.ec2.internal>
2025-09-01 09:38:06 +0000

    updates

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * void more_numbers - function print 10 times numbers between 0 to 14
+ *
+ * Return: numbers between 0 to 14 * 10 times
+ *
+ */
+
+int more_numbers(void)
+{
+
+  int x;
+  for (x=1; x <= 10; x++)
+    {
+      int l;
+      for (l=0; l<= 14; l++)
+	{
+	  _putchar(l);
+	}
+      _putchar("\n")
+      
+    }
+  _putchar("\n")
+  
+}
