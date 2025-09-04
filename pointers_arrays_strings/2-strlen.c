71b197ef0631000d062a162c943a51f2c22a530e
root <root@ip-10-42-59-190.ec2.internal>
2025-09-04 23:37:11 +0000

    updates

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -8,10 +8,12 @@
   */
 int _strlen(char *s)
 {
-  int c;
-  for (c=0 ; *s != '\0'; s++)
-    {
-      c++;
-    }
-  return (c);
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
 }
