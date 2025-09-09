40fc5cdf3bcf3116af72543c58059f4ac282509e
root <root@ip-10-42-175-236.ec2.internal>
2025-09-09 23:21:21 +0000

    updates

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- *
- * _print_rev_recursion - prints a string in reverse
- * @*s: the value will be reversed
- *
- * Return: @s in reverse
- *
- */
- _print_rev_recursion(char *s)
+  * _print_rev_recursion - Print a string in reverse
+  * @s: the string to reverse
+  *
+  * Return: Nothing.
+  */
+void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-  return;
-}
+	if (*s == '\0')
+	{
+		return;
+	}
 
-s++;
-_print_rev_recursion(s);
-s--;
-_putchar(*s);
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
