177124ab04e593db929eece68f97802e130a72ea
root <root@ip-10-42-175-236.ec2.internal>
2025-09-09 23:34:00 +0000

    updates

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- *
- * _strlen_recursion - function that returns the length of a string
- * @*s: a string to count
- *
- * Return: length of a string
- */
+  * _strlen_recursion - Calculate the length of a string
+  * @s: the string to count
+  *
+  * Return: integer value
+  */
 int _strlen_recursion(char *s)
-{  
-  if (*s == '\0')
-    {
-      return (0);
-    }
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
 
-  s++;
-  return (_strlen_recursion(s) + 1);
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
