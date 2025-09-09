d61dfb602fb8cb99b3f39bd0cbc65e960d7e022b
root <root@ip-10-42-175-236.ec2.internal>
2025-09-09 23:16:41 +0000

    updates

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -10,13 +10,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-  if (*s == '\0')
-    {
-      return;  
-    }
-  
-    s++;
-  _print_rev_recursion(s);
-  s--;
-  _putchar(*s);
+if (*s == '\0')
+{
+return;  
+}
+ 
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
 }
