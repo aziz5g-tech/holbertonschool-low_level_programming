de28e0b42f3c9d9d55d82afcd220ee37dd7e0cfe
root <root@ip-10-42-175-236.ec2.internal>
2025-09-09 23:32:17 +0000

    updates

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,14 +8,12 @@
  * Return: length of a string
  */
 int _strlen_recursion(char *s)
-{
-  int n = 0;
-  
+{  
   if (*s == '\0')
     {
       return n;
     }
 
-  n++;
-  _strlen_recursion(s);
+  s++;
+  return (_strlen_recursion(s) + 1);
 }
