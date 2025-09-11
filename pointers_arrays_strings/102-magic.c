41b15c15b8cba5fc451fee0a7abdb5fb053ac58c
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 19:08:39 +0000

    updates

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -3,27 +3,20 @@
 /**
  * print_number - Prints an integer using only _putchar
  * @n: The integer to print
- *
- * Description: This function handles negative numbers,
- * avoids using long, arrays, pointers, or hard-coded values.
  */
 void print_number(int n)
 {
-	int digit;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		if (n / 10 != 0)
+		if (n / 10)
 			print_number(-(n / 10));
-		digit = -(n % 10);
+		_putchar(-(n % 10) + '0');
 	}
 	else
 	{
-		if (n / 10 != 0)
+		if (n / 10)
 			print_number(n / 10);
-		digit = n % 10;
+		_putchar((n % 10) + '0');
 	}
-
-	_putchar(digit + '0');
 }
