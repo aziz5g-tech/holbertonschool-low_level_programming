7f095ecf0803b2e3bf7a9a9e3ca5ff3a94a674ab
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 15:02:30 +0000

    updates

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[]) {
+    printf("%s\n", argv[0]);
+    return 0;
+}
