c9b6507a1d86baaa519d4cd51706ff3eb9c366a3
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 18:32:27 +0000

    updates

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,18 +9,20 @@
   */
 void print_number(int n)
 {
-	unsigned int i;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
+		n *= -1;
+		k = n;
 		_putchar('-');
-		i = -n;
 	}
-	else
-	{
-		i = n;
-	}
-	if (i / 10 != 0)
-		print_number(i / 10);
-	_putchar((i % 10) + '0');
+
+	k /= 10;
+
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+
 }
