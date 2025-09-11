06e25ed168d8ae12b9072fbbfb87e7136a6db249
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 19:25:28 +0000

    updates

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,22 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_number - Prints an integer using only _putchar
- * @n: The integer to print
- */
-void print_number(int n)
+int main(void)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		if (n / 10)
-			print_number(n / 10 * -1);
-		_putchar(n % 10 * -1 + 48);
-	}
-	else
-	{
-		if (n / 10)
-			print_number(n / 10);
-		_putchar(n % 10 + 48);
-	}
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
