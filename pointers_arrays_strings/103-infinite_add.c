834c7f92eda1648ac833eef12f46fbe19856a68f
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 19:26:34 +0000

    updates

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,45 +1,54 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * infinite_add - Adds two numbers stored as strings
- * @n1: First number
- * @n2: Second number
- * @r: Buffer to store result
- * @size_r: Size of buffer
- *
- * Return: Pointer to result or 0 if result can't fit
- */
+* infinite_add - adds two numbers
+* @n1: number one.
+* @n2: number two.
+* @r: buffer that the function will use to store the result.
+* @size_r: buffer size:
+* Return: the pointer to dest.
+*/
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k, l, sum, carry = 0;
+int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-
-	if (i > size_r - 1 || j > size_r - 1)
-		return (0);
-
-	k = i - 1;
-	l = j - 1;
-	r[size_r - 1] = '\0';
-	size_r--;
-
-	while (k >= 0 || l >= 0 || carry)
-	{
-		if (size_r == 0)
-			return (0);
-
-		sum = carry;
-		if (k >= 0)
-			sum += n1[k--] - '0';
-		if (l >= 0)
-			sum += n2[l--] - '0';
-
-		r[--size_r] = (sum % 10) + '0';
-		carry = sum / 10;
-	}
-
-	return (&r[size_r]);
+while (*(n1 + c1) != '\0')
+c1++;
+while (*(n2 + c2) != '\0')
+c2++;
+if (c1 >= c2)
+bg = c1;
+else
+bg = c2;
+if (size_r <= bg + 1)
+return (0);
+r[bg + 1] = '\0';
+c1--, c2--, size_r--;
+dr1 = *(n1 + c1) -48, dr2 = *(n2 + c2) -48;
+while (bg >= 0)
+{
+op = dr1 + dr2 + add;
+if (op >= 10)
+add = op / 10;
+else
+add = 0;
+if (op > 0)
+*(r + bg) = (op % 10) + 48;
+else
+*(r + bg) = '0';
+if (c1 > 0)
+c1--, dr1 = *(n1 + c1) -48;
+else
+dr1 = 0;
+if (c2 > 0)
+c2--, dr2 = *(n2 + c2) -48;
+else
+dr2 = 0;
+bg--, size_r--;
+}
+if (*(r) == '0')
+return (r + 1);
+else
+return (r);
 }
