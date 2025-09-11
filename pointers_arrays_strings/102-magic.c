ed8fe9783ac24b7f24d90c08cf447ee3285d77ec
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 19:09:56 +0000

    updates

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -11,12 +11,12 @@ void print_number(int n)
 		_putchar('-');
 		if (n / 10)
 			print_number(-(n / 10));
-		_putchar(-(n % 10) + '0');
+		_putchar('0' + -(n % 10));
 	}
 	else
 	{
 		if (n / 10)
 			print_number(n / 10);
-		_putchar((n % 10) + '0');
+		_putchar('0' + (n % 10));
 	}
 }
