c475cf89d867b54a549eaaad7698d81b1af964cb
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 18:53:45 +0000

    updates

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using only _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	int digit;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n / 10 != 0)
+			print_number(-(n / 10));
+		digit = -(n % 10);
+	}
+	else
+	{
+		if (n / 10 != 0)
+			print_number(n / 10);
+		digit = n % 10;
+	}
+
+	_putchar(digit + '0');
+}
