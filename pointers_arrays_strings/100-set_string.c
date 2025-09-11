cac4a0c119f30a87c4e0ff1cb633fcd59d2c86ab
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 19:35:45 +0000

    updates

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * set_string - Sets the value of a pointer to a char.
- * @s: The pointer.
- * @to: The char.
+ * set_string - Sets the value of a pointer to a char
+ * @s: Pointer to pointer to char
+ * @to: Pointer to char to assign
  */
 void set_string(char **s, char *to)
 {
