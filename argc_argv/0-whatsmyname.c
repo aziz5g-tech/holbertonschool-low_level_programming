bcba452f8429dc4f51705e83b15ed46e320bc1c0
root <root@ip-10-42-228-165.ec2.internal>
2025-09-11 15:04:34 +0000

    updates

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,7 +7,10 @@
   *
   * Return: Always zero
   */
-int main(int argc, char *argv[]) {
-    printf("%s\n", argv[0]);
-    return 0;
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
