e13a257be2525b684587e8f35a431f6834afeb59
root <root@ip-10-42-166-125.ec2.internal>
2025-09-16 22:32:48 +0000

    updates

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#inlcude <stdlib.h>
+
+/**
+ *
+ * create_array - the function to use a specific char
+ * @size: number of array size
+ * @c: the char will be used in array
+ *
+ * Return: The array filled
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+  unsigned int i;
+  char *s;
+
+  if (size == 0)
+    return (NULL);
+  
+    s = malloc(size * sizeof(char));
+
+    if (s == NULL)
+      return (NULL);
+
+    for (i = 0; i < size; i++)
+      {
+	s[i] = c;
+      }
+
+    return (s);
+}
