4c1ea86c6c172fe132e2e590ce0b555126bb4ae6
root <root@ip-10-42-166-125.ec2.internal>
2025-09-16 22:43:05 +0000

    updates

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,21 +12,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *s;
+	unsigned int i;
+	char *s;
 
 	if (size == 0)
 		return (NULL);
-	
-    s = malloc(size * sizeof(char));
 
-    if (s == NULL)
-      return (NULL);
+	s = malloc(size * sizeof(char));
 
-    for (i = 0; i < size; i++)
-      {
-	s[i] = c;
-      }
+	if (s == NULL)
+		return (NULL);
 
-    return (s);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
 }
