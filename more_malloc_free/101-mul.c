bba75b08dc157502e8a68fe1f85d9272d25a58b4
root <root@ip-10-42-210-247.ec2.internal>
2025-09-19 23:55:53 +0000

    updates

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -34,9 +34,7 @@ int _strlen(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
 
@@ -59,43 +57,61 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, j, carry, digit1, digit2, *result, started = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	if (argc != 3)
 		errors();
+
+	s1 = argv[1];
+	s2 = argv[2];
+
+	if (!is_digit(s1) || !is_digit(s2))
+		errors();
+
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
+
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
+
+	/* Initialize result array to 0 */
+	for (i = 0; i < len; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+
+	/* Multiply each digit */
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = s1[i] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = s2[j] - '0';
+			carry += result[i + j + 1] + (digit1 * digit2);
+			result[i + j + 1] = carry % 10;
 			carry /= 10;
 		}
+
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			result[i + j + 1] += carry;
 	}
+
+	/* Print the result */
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
-			a = 1;
-		if (a)
+			started = 1;
+		if (started)
 			_putchar(result[i] + '0');
 	}
-	if (!a)
+
+	if (!started)
 		_putchar('0');
+
 	_putchar('\n');
 	free(result);
+
 	return (0);
 }
