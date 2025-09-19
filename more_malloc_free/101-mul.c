516a08eac57bcfc29e45e23b64343342ff3d5bdb
root <root@ip-10-42-210-247.ec2.internal>
2025-09-19 23:58:00 +0000

    updates

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ERR_MSG "Error"
-
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * is_digit - checks if a string contains only digits
+ * @s: string to check
+ * Return: 1 if string has only digits, 0 otherwise
  */
 int is_digit(char *s)
 {
 	int i = 0;
+
+	if (!s)
+		return (0);
 
 	while (s[i])
 	{
@@ -26,80 +26,71 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
- * Return: the length of the string
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 		i++;
 	return (i);
 }
 
 /**
- * errors - handles errors for main
+ * print_error - prints Error and exits with status 98
  */
-void errors(void)
+void print_error(void)
 {
 	printf("Error\n");
 	exit(98);
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ * init_result - initializes an int array to 0
+ * @result: array to initialize
+ * @size: size of array
  */
-int main(int argc, char *argv[])
+void init_result(int *result, int size)
 {
-	char *s1, *s2;
-	int len1, len2, len, i, j, carry, digit1, digit2, *result, started = 0;
+	int i;
+	for (i = 0; i < size; i++)
+		result[i] = 0;
+}
 
-	if (argc != 3)
-		errors();
-
-	s1 = argv[1];
-	s2 = argv[2];
-
-	if (!is_digit(s1) || !is_digit(s2))
-		errors();
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
+/**
+ * multiply - multiplies two positive numbers
+ * @num1: first number as string
+ * @num2: second number as string
+ */
+void multiply(char *num1, char *num2)
+{
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
+	int len = len1 + len2;
+	int *result, i, j, carry, digit1, digit2, started = 0;
 
 	result = malloc(sizeof(int) * len);
 	if (!result)
-		return (1);
+		exit(1);
 
-	/* Initialize result array to 0 */
-	for (i = 0; i < len; i++)
-		result[i] = 0;
+	init_result(result, len);
 
-	/* Multiply each digit */
 	for (i = len1 - 1; i >= 0; i--)
 	{
-		digit1 = s1[i] - '0';
+		digit1 = num1[i] - '0';
 		carry = 0;
-
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			digit2 = s2[j] - '0';
+			digit2 = num2[j] - '0';
 			carry += result[i + j + 1] + (digit1 * digit2);
 			result[i + j + 1] = carry % 10;
 			carry /= 10;
 		}
-
-		if (carry > 0)
-			result[i + j + 1] += carry;
+		result[i + j + 1] += carry;
 	}
 
-	/* Print the result */
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (result[i])
 			started = 1;
@@ -112,6 +103,23 @@ int main(int argc, char *argv[])
 
 	_putchar('\n');
 	free(result);
+}
+
+/**
+ * main - entry point, multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		print_error();
+
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+		print_error();
+
+	multiply(argv[1], argv[2]);
 
 	return (0);
 }
