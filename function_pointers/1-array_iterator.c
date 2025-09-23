411c712b454e2bec7d32be62826395091ccdc051
root <root@ip-10-42-141-252.ec2.internal>
2025-09-23 23:27:25 +0000

    updates

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-  * array_iterator - function that executes a function given as a parameter on each element of an array
-  * @array: the array
-  * @size: the size of the array
-  * @action: a pointer to the function
+  * array_iterator - ...
+  * @array: ...
+  * @size: ...
+  * @action: ...
   *
-  * Return: elements of array
+  * Return: ...
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
