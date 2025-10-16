607e4ea0b9cd2078379f3e00d81a6052ab96774f
root <root@ip-10-42-40-106.ec2.internal>
2025-10-16 01:51:50 +0000

    feat: 100-first.c to know how to print something before main fucntion

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,11 +3,12 @@
 void do_it_first(void) __attribute__ ((constructor));
 
 /**
-  * first - print You're beat! and yet, you must allow,\nI bore my house upon my back!\n
+  * first - print You're beat! and yet, you must allow,\nI bore my...ets
   *
   * Return: prints before main
   */
 void do_it_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+  printf("You're beat! and yet, you must allow,\n");
+  printf("I bore my house upon my back!\n");
 }
